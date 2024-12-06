12d3cf784cf5ff85f3596459c8e382f070a4c5b5
iamvetrioffl <166475004+iamvetrioffl@users.noreply.github.com>
2024-12-06 17:34:06 +0530

    Add files via upload

---
+++ user.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css"/>
+
+    <title>Project</title>
+  </head>
+  <body>
+    <h1>Hellow World</h1>
+
+
+  </body>
+</html>
